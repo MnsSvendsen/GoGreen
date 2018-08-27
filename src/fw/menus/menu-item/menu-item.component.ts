0cc8dc32a2b0c64baa9a6423296ebc944fd1f77d
mnssvendsen <mnssvendsen@gmail.com>
2018-08-27 14:34:28 +0200

    reworked routing for menu

--- src/fw/menus/menu-item/menu-item.component.ts
+++ src/fw/menus/menu-item/menu-item.component.ts
@@ -1,40 +1,80 @@
-import { Component, OnInit, Input, HostBinding, HostListener } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, HostListener, ElementRef, Renderer } from '@angular/core';
 
 import {MenuItem, MenuService} from '../../services/menu.service'
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+
+
 @Component({
   selector: 'fw-menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.css']
 })
 export class MenuItemComponent implements OnInit {
-  @Input() item: MenuItem;
+  @Input() item = <MenuItem>null;  // see angular-cli issue #2034
   @HostBinding('class.parent-is-popup')
   @Input() parentIsPopup = true;
   isActiveRoute = false;
-  
+
   mouseInItem = false;
   mouseInPopup = false;
   popupLeft = 0;
   popupTop = 34;
 
-  constructor(private router:Router,
-    private menuService : MenuService) { }
-
-  ngOnInit(): void {
+  constructor(private router:Router, 
+              private menuService: MenuService,
+              private el: ElementRef,
+              private renderer: Renderer) {
   }
-  
-  onPopupMouseEnter(event) : void {
-    if (!this.menuService.isVertical) {
-        this.mouseInPopup = true;
-    }
-}
 
-onPopupMouseLeave(event) : void {
-    if (!this.menuService.isVertical) {
-        this.mouseInPopup = false;
+  checkActiveRoute(route: string) {
+    this.isActiveRoute = (route == '/' + this.item.route);
+  }
+
+  ngOnInit() : void {
+    this.checkActiveRoute(this.router.url);
+
+    this.router.events
+        .subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                this.checkActiveRoute(event.url);
+                //console.log(event.url + ' ' + this.item.route + ' ' + this.isActiveRoute);
+            }
+        });
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event) : void {
+
+    event.stopPropagation();
+
+    if (this.item.submenu) {
+      if (this.menuService.isVertical) {
+          this.mouseInPopup = !this.mouseInPopup;
+      }
     }
-}
+    else if (this.item.route) {
+      // force horizontal menus to close by sending a mouseleave event
+      let newEvent = new MouseEvent('mouseleave', {bubbles: true});
+      this.renderer.invokeElementMethod(
+          this.el.nativeElement, 'dispatchEvent', [newEvent]);
+
+      this.router.navigate(['/' + this.item.route]);
+        
+    }
+  }
+
+  onPopupMouseEnter(event) : void {
+      if (!this.menuService.isVertical) {
+          this.mouseInPopup = true;
+      }
+  }
+
+  onPopupMouseLeave(event) : void {
+      if (!this.menuService.isVertical) {
+          this.mouseInPopup = false;
+      }
+  }
+
   @HostListener('mouseleave', ['$event'])
   onMouseLeave(event) : void {
       if (!this.menuService.isVertical) {
@@ -54,5 +94,4 @@ onPopupMouseLeave(event) : void {
           }
       }
   }
-
 }
