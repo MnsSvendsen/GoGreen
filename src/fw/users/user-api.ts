e667371c70424565a086b3b61795769f80bf1813
mnssvendsen <mnssvendsen@gmail.com>
2018-08-30 10:38:44 +0200

    App runs on agular 2 and need to be updatet to angular 4 or later

--- src/fw/users/user-api.ts
+++ src/fw/users/user-api.ts
@@ -3,5 +3,5 @@ import { Observable } from "rxjs";
 
 export abstract class UserApi {
     signIn: (username: string, password: string, rememberMe: boolean) => Observable<any>;
-    
+    signOut: () => Observable<any>;
 }
